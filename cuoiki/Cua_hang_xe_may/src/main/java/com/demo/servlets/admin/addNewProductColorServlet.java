7f793ef22873dc7c53e8bbcf88f2cb2f1004f136
Trung Tran <21130590@st.hcmuaf.edu.vn>
2023-12-24 21:46:44 +0700

    delete and created

--- cuoiki/Cua_hang_xe_may/src/main/java/com/demo/servlets/admin/addNewProductColorServlet.java
+++ cuoiki/Cua_hang_xe_may/src/main/java/com/demo/servlets/admin/addNewProductColorServlet.java
@@ -2,11 +2,23 @@ package com.demo.servlets.admin;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+import com.demo.entities.ProductColor;
+import com.demo.helpers.UploadFileHelper;
+import com.demo.models.ColorModel;
 @WebServlet("/admin/addNewProductColor")
+@MultipartConfig(
+		
+		maxFileSize = 1024 * 1024 * 5 ,
+		maxRequestSize = 1024 * 1024 * 10,
+		fileSizeThreshold = 1024 * 1024 * 10
+)
 /**
  * Servlet implementation class addNewProductColor
  */
@@ -35,7 +47,34 @@ public class addNewProductColorServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		String action = request.getParameter("action");
+		System.out.println(action);
+		if(action.equalsIgnoreCase("addnewcolor")) {
+			doPost_newColor(request, response);
+			
+		}
+	}
+	protected void doPost_newColor(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int idProduct = Integer.parseInt(request.getParameter("idProduct"));
+		int idVersion =  Integer.parseInt(request.getParameter("idVersion"));
+		String color1 = request.getParameter("color");
+		String valueColor = request.getParameter("valueColor");
+		double price = Double.parseDouble(request.getParameter("priceColor"));
+		Part photo = request.getPart("avatar");
+		String avatar = UploadFileHelper.uploadFile("assets/user/Image", request, photo);
+		ProductColor color = new ProductColor();
+		color.setVersionID(idVersion);
+		color.setColor(color1);
+		color.setPrice(price);
+		color.setPhoto(avatar);
+		color.setValue(valueColor);
+		ColorModel colorModel = new ColorModel();
+		if(colorModel.create(color)) {
+			response.sendRedirect("productcolor");
+		} else {
+			System.out.println("messageError");
+		}
+		
 	}
 
 }
